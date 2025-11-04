069b5c657b69af130f856e461a2134942a085ba4
marcuswonder <marcus.bruce.smith@gmail.com>
2025-11-04 21:34:22 +0000

    Updated option-notes to match Zakeke backend

--- src/data/option-notes.tsx
+++ src/data/option-notes.tsx
@@ -1,20 +1,11 @@
 export const optionNotes: Record<string, Record<string, string>> = {
-    bottles: {
-        "Manila": "The iconic pharmaceutical style is inspired by the harmony of Japanese paper umbrellas",
-        "Antica": "Inspired by the former apothecary era, this timeless shape seduces with its charm and pure simplicity.",
-        "Outlaw": "Retro-inspired with a nostalgic charm. Outlaw is understated, practical, and tells a frontier story.",
-        "Origin": "Elegant and iconic. The refined lines, evoking vintage-style atmospheres, bring a timeless charm.",
-        "Polo": "Strong and stable, with a heavy glass base and rounded shoulders, Polo is a classic bottle with a timeless style.",
-    },
     liquids: {
-        "Contemporary Gin": "This contemporary London dry gin opens with a vibrant burst of citrus that invigorates the palate, leading into rich juniper notes. The experience concludes with a warming touch of spice, creating a balanced and satisfying finish.",
-        "Citrus Gin": "This citrus gin boasts a full-bodied character, bursting with refreshing and zesty notes of kaffir lime leaves, lemongrass, and lemon peel. The finish is complemented by hints of grassy and piney botanicals, creating a harmonious balance that lingers on the palate.",
+        "London Dry Gin": "This classic London dry gin delivers a strong juniper flavor complemented by fresh citrus and earthy spice notes. The finish is clean and crisp, with a lingering herbal essence that adds depth.",
         "Pink Gin": "This pink gin presents a light and delicate profile, starting with bright juniper that's beautifully softened by the gentle sweetness of blueberries and rhubarb. It’s rounded off with refreshing citrus notes of orange and lemon, adding a vibrant zing to the finish.",
-        "Classic Gin": "This classic London dry gin delivers a strong juniper flavor complemented by fresh citrus and earthy spice notes. The finish is clean and crisp, with a lingering herbal essence that adds depth.",
-        "Wild Berry": "This gin features a smooth juniper-forward profile enhanced by layers of creamy lemon myrtle and peppermint bark, complemented by warming spices. The experience concludes with a subtle richness from forest berries and cherries, adding depth and complexity to each sip.",
+        "Wild Berry Gin": "This gin features a smooth juniper-forward profile enhanced by layers of creamy lemon myrtle and peppermint bark, complemented by warming spices. The experience concludes with a subtle richness from forest berries and cherries, adding depth and complexity to each sip.",
     },
     closures: {
-        "Wood": "Wooden Closure",
-        "Wax Sealed": "Wooden Closure with Wax Seal",
+        "Light Wood": "A natural beech stopper with a synthetic stem.",
+        "Wax Sealed": "A black wood stopper with a synthetic stem.",
     }
     };
